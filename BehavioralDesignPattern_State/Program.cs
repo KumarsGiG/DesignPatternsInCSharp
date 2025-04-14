a1a5de33060179b555e9d2fc118e47e55ca3e993
Kumars <kumarschrome@gmail.com>
2025-04-14 16:51:54 +0530

    BehavioralDesignPattern_State final

---
+++ BehavioralDesignPattern_State/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BehavioralDesignPattern_State
+{
+    #region ## STATE INTRODUCTION ##
+
+    /// <summary>
+    /// State is a behavioral design pattern that allows an object to change the behavior when its internal state changes.
+    /// </summary>
+
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var context = new Context(new ConcreteStateA());
+            context.Request1();
+            context.Request2();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BehavioralDesignPattern_State/State.cs
@@ -0,0 +1,21 @@
+﻿namespace BehavioralDesignPattern_State
+{
+    // The base State class declares methods that all Concrete State should
+    // implement and also provides a backreference to the Context object,
+    // associated with the State. This backreference can be used by States to
+    // transition the Context to another State.
+    public abstract class State
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        protected Context _context;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+        public void SetContext(Context context)
+            => this._context = context;
+
+        public abstract void Handle1();
+
+        public abstract void Handle2();
+
+    }
+}
