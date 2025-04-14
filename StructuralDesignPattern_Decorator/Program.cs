1aa8c7de0b1698aa85e28058d821f010319399f4
Kumars <kumarschrome@gmail.com>
2025-04-14 18:20:10 +0530

    StructuralDesignPattern_Decorator final

--- StructuralDesignPattern_Decorator/Program.cs
+++ StructuralDesignPattern_Decorator/Program.cs
@@ -13,7 +13,7 @@
     {
         static void Main(string[] args)
         {
-            Client client = new Client();
+            var client = new Client();
 
             var simple = new ConcreteComponent();
             Console.WriteLine("Client: I get a simple component:");
@@ -24,8 +24,8 @@
             //
             // Note how decorators can wrap not only simple components but the
             // other decorators as well.
-            ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
-            ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+            var decorator1 = new ConcreteDecoratorA(simple);
+            var decorator2 = new ConcreteDecoratorB(decorator1);
             Console.WriteLine("Client: Now I've got a decorated component:");
             client.ClientCode(decorator2);
 
