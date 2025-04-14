3fc40fd714e8f602e8dec06889c8908edda2fbd3
Kumars <kumarschrome@gmail.com>
2025-04-14 17:00:08 +0530

    BehavioralDesignPattern_Strategy final

---
+++ BehavioralDesignPattern_Strategy/ConcreteStrategyA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehavioralDesignPattern_Strategy
+{
+    public class ConcreteStrategyA : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            return list;
+        }
+    }
+}
