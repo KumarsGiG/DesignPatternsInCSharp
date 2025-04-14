d551269ac88c33f844069b9636278f579a3bd58b
Kumars <kumarschrome@gmail.com>
2025-04-14 17:17:24 +0530

    BehavioralDesignPattern_TemplateMethod final

---
+++ BehavioralDesignPattern_TemplateMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BehavioralDesignPattern_TemplateMethod
+{
+    #region ## TEMPLATE METHOD INTRODUCTION ##
+
+    /// <summary>
+    /// Template Method is a behavioral design pattern that allows you to define a skeleton
+    /// of an algorithm in a base class and let subclasses override the steps without changing the overall algorithm’s structure.
+    /// </summary>
+
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Same client code can work with different subclasses:");
+
+            Client.ClientCode(new ConcreteClass1());
+
+            Console.Write("\n");
+
+            Console.WriteLine("Same client code can work with different subclasses:");
+            Client.ClientCode(new ConcreteClass2());
+
+            Console.ReadLine();
+        }
+    }
+}
