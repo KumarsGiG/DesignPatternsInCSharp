505377f7e59de4e6d76ec93716335ec49f0a6ab0
Kumars <kumarschrome@gmail.com>
2025-04-15 20:30:24 +0530

    StructuralDesignPattern_Proxy final

---
+++ StructuralDesignPattern_Proxy/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StructuralDesignPattern_Proxy
+{
+    #region ## PROXY INTRODUCTION ##
+
+    /// <summary>
+    /// Proxy is a structural design pattern that provides an object that acts as a substitute for a real service object used by a client. 
+    /// A proxy receives client requests, does some work (access control, caching, etc.) and then passes the request to a service object.
+    /// </summary>
+
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client();
+
+            Console.WriteLine("Client: Executing the client code with a real subject:");
+            var realSubject = new RealSubject();
+            client.ClientCode(realSubject);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Executing the same client code with a proxy:");
+            var proxy = new Proxy(realSubject);
+            client.ClientCode(proxy);
+
+            Console.ReadLine();
+        }
+    }
+}
