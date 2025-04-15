072ff92849e5818a35f43ec768ed9b902fe9f4a5
Kumars <kumarschrome@gmail.com>
2025-04-15 22:27:59 +0530

    CreationalDesignPattern_FactoryMethod final

--- CreationalDesignPattern_FactoryMethod/Client.cs
+++ CreationalDesignPattern_FactoryMethod/Client.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("App: Launched with the ConcreteCreator1.");
             ClientCode(new ConcreteCreator1());
 
-            Console.WriteLine("");
+            Console.WriteLine(string.Empty);
 
             Console.WriteLine("App: Launched with the ConcreteCreator2.");
             ClientCode(new ConcreteCreator2());
