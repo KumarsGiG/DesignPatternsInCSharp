5b8bd7b089a7c1039f7e39ea71b690dedd881b7e
Kumars <kumarschrome@gmail.com>
2025-04-15 21:21:35 +0530

    CreationalDesignPattern_Builder final

---
+++ CreationalDesignPattern_Builder/IBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace CreationalDesignPattern_Builder
+{
+    // The Builder interface specifies methods for creating the different parts
+    // of the Product objects.
+    public interface IBuilder
+    {
+        void BuildPartA();  void BuildPartB();  void BuildPartC();
+    }
+}
